b9450d84d074168c54e9abfcb2d86be7f50bb8ff
Alexis <lexisadams92@gmail.com>
2021-04-01 20:06:19 -0400

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function saturdayFun (activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork (willDo = 'go to the office') {
+    return (`This Monday, I will ${willDo}.`);
+}
+
+function wrapAdjective(string='*') {
+    return function(param='special') {
+        return 'You are ' + string + param + string + '!'
+    }
+}
+
+const Calculator = Object.assign({}, {
+    add: function(a, b) { return a + b },
+    subtract: function(a, b) { return a - b },
+    multiply: function(a, b) { return a * b },
+    divide: function(a, b) { return a / b }
+})
+
+function actionApplyer(startInt, array) {
+    array.forEach( f => startInt = f(startInt))
+    return startInt
+}
